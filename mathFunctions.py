fc1cecc17944b76f0f71eb315ccda87779bdb4f2
mahmels <el-sayed.mahmoud@hotmail.com>
2024-04-08 16:56:20 +0200

    Update mathFunctions.py

--- mathFunctions.py
+++ mathFunctions.py
@@ -10,9 +10,29 @@ def calculate_mean(a, b):
 
 def calculate_power(base, exp):
     """
-    Calculate the power of a raised to the given exponent
+    calculate the power of a raised to the given exponent
     :param base: base
     :param exp: exponent
     :return: power
     """
     return base ** exp
+
+
+def is_prime(num):
+    """
+    check if num is prime
+    :param num: number to be checked
+    :return: true if num is prime, false otherwise
+    """
+    if num <= 1:
+        return False
+    elif num <= 3:
+        return True
+    elif num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
