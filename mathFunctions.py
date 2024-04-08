aa7cf45d8959355958167ec64c2a741f0a9d6f93
mahmels <el-sayed.mahmoud@hotmail.com>
2024-04-08 16:49:15 +0200

    Start mathFunctions.py

---
+++ mathFunctions.py
@@ -0,0 +1,18 @@
+def calculate_mean(a, b):
+    """
+    calculate the mean of a and b
+    :param a: first
+    :param b: second
+    :return: mean
+    """
+    return float(a + b) / 2
+
+
+def calculate_power(base, exp):
+    """
+    Calculate the power of a raised to the given exponent
+    :param base: base
+    :param exp: exponent
+    :return: power
+    """
+    return base ** exp
